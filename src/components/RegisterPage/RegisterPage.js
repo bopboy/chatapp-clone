330bc28455708f5a7606e56b22f5f3f1bee1a7f4
bopboy2 <scp0000@gmail.com>
2021-10-07 22:43:02 +0900

    회원가입 정보 데이터베이스 저장

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import md5 from 'md5';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { getDatabase, ref, set } from 'firebase/database';
 import firebase from '../../firebase'
 
 
@@ -10,13 +12,26 @@ function RegisterPage() {
     const password = useRef()
     password.current = watch("password")
     const [errorFromSubmit, seterrorFromSubmit] = useState("")
+    const [loading, setLoading] = useState(false)
     const onSubmit = async (data) => {
         try {
+            setLoading(true)
             const auth = getAuth(firebase);
             const createdUser = await createUserWithEmailAndPassword(auth, data.email, data.password)
             console.log('createdUser', createdUser)
+            setLoading(false)
+            await updateProfile(createdUser.user, {
+                displayName: data.name,
+                photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+            })
+            const database = getDatabase(firebase)
+            await set(ref(database, 'users/' + createdUser.user.uid), {
+                name: createdUser.user.displayName,
+                image: createdUser.user.photoURL
+            }).then(() => { alert('회원 가입 정보 저장 성공') }).catch((e) => { console.log(e) })
         } catch (error) {
             seterrorFromSubmit(error.message)
+            setLoading(false)
             setTimeout(() => { seterrorFromSubmit("") }, 5000)
         }
     }
@@ -39,11 +54,11 @@ function RegisterPage() {
                 {errors.password && errors.password.type === 'minLength' && <p>비번은 최소 6자여야 합니다</p>}
 
                 <label>Password Confirm</label>
-                <input type="passowrd" name="password_confirm" {...register("password_confirm", { required: true, validate: v => v === password.current })} />
+                <input type="password" name="password_confirm" {...register("password_confirm", { required: true, validate: v => v === password.current })} />
                 {errors.password_confirm && errors.password_confirm.type === 'required' && <p>비번 재입력은 필수입니다</p>}
                 {errors.password_confirm && errors.password_confirm.type === 'validate' && <p>비번과 재입력 비번이 다릅니다</p>}
                 {errorFromSubmit && <p>{errorFromSubmit}</p>}
-                <input type="submit" value="SUBMIT" />
+                <input type="submit" value="SUBMIT" disabled={loading} />
                 <Link style={{ color: 'gray', textDecoration: 'none' }} to="login">이미 아이디가 있다면...</Link>
             </form>
         </div>
