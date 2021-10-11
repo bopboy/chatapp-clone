102ed3fdb55ef5130a9a2a593cb471d276b097ec
bopboy2 <scp0000@gmail.com>
2021-10-11 10:01:06 +0900

    채팅 페이지 기본 UI 작성

---
+++ src/components/ChatPage/SidePanel/SadePanel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import UserPanel from './UserPanel'
+import Favorited from './Favorited'
+import ChatRooms from './ChatRooms'
+import DirectMessage from './DirectMessage'
+
+function SadePanel() {
+    return (
+        <div style={{ backgroundColor: "#7B83E8", padding: '2rem', minHeight: '100vh', color: 'white', minWidth: '275px' }}>
+            <UserPanel />
+            <Favorited />
+            <ChatRooms />
+            <DirectMessage />
+        </div>
+    )
+}
+
+export default SadePanel
