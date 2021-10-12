5927d3326a9021d7e299d05f53c16a58bc1e7d30
bopboy2 <scp0000@gmail.com>
2021-10-13 07:07:52 +0900

    메시지 헤더, 폼 UI 작성

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Form, ProgressBar, Row, Col } from 'react-bootstrap'
 
 function MessageForm() {
     return (
         <div>
-            MessageForm
+            <Form>
+                <Form.Group contrlId="exampleForm.ControlTextarea1">
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+            </Form>
+            <ProgressBar variant="warning" label="60%" now={60} style={{ marginTop: '1rem' }} />
+            <Row>
+                <Col>
+                    <button style={{ width: '100%' }} className="message-form-button">SEND</button>
+                </Col>
+                <Col>
+                    <button style={{ width: '100%' }} className="message-form-button">UPLOAD</button>
+                </Col>
+            </Row>
         </div>
     )
 }
--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,10 +1,58 @@
 import React from 'react'
-
+import { Container, Row, Col, InputGroup, FormControl, Image, Accordion, Card, Button } from 'react-bootstrap'
+import { FaLock } from 'react-icons/fa'
+import { MdFavorite } from 'react-icons/md'
+import { AiOutlineSearch } from 'react-icons/ai'
 function MessageHeader() {
     return (
-        <div>
-            MessageHeader
-        </div>
+        <div style={{ width: '100%', height: '172px', border: '.2rem solid #ececec', borderRadius: '4px', padding: '1rem', marginBottom: '1rem' }}>
+            <Container>
+                <Row>
+                    <Col><h2><FaLock /> Chat Room Name <MdFavorite /></h2></Col>
+                    <Col>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1"><h5><AiOutlineSearch /></h5></InputGroup.Text>
+                            <FormControl
+                                placeholder="Search Message"
+                                aria-label="Search"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <p>
+                        <Image src="" /> {" "} user name
+                    </p>
+                </div>
+                <Row>
+                    <Col>
+                        <Accordion flush>
+                            <Card>
+                                <Card.Header style={{ padding: '0 1rem' }}>
+                                    <Accordion.Toggle as={Button} variant="link" eventKey="0" style={{ textDecoration: 'none' }}>Click me!</Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>Hello! I'm the body</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </Col>
+                    <Col>
+                        <Accordion>
+                            <Card>
+                                <Card.Header style={{ padding: '0 1rem' }}>
+                                    <Accordion.Toggle as={Button} variant="link" eventKey="0" style={{ textDecoration: 'none' }}>Click me!</Accordion.Toggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey="0">
+                                    <Card.Body>Hello! I'm the body</Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        </Accordion>
+                    </Col>
+                </Row>
+            </Container>
+        </div >
     )
 }
 
